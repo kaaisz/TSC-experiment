0adfc311adf44bea11866104685dd56d53c3d230
Kaai Suzuki <suzydp@me.com>
2019-05-11 14:55:36 -0700

    Add example1.ts

---
+++ example1.ts
@@ -0,0 +1,10 @@
+//define types
+
+let exampleA: any; //you can apply any kind of type here
+
+//both side should match with the data type you defined
+let example1: boolean = true; 
+
+let example2: number = 35;
+
+let example3: string = 'Hello World';
