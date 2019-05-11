2595eec1b0fe5207e9399c4f3b8929331affe93a
Kaai Suzuki <suzydp@me.com>
2019-05-11 15:29:03 -0700

    Add some model codes on example1.ts

--- example1.ts
+++ example1.ts
@@ -4,8 +4,15 @@ let exampleA: any; //you can apply any kind of type here
 
 //both side should match with the data type you defined
 //you can add optional data type by using `|`
-let example1: boolean | number = true; 
 
-let example2: number = 35;
+const arrayExample = [1, 3, 4, 5];
+
+let example1: boolean | number = 35; 
+
+let example2: number;
 
 let example3: string = 'Hello World';
+
+let example4: undefined = undefined;
+
+let example5: null = null;
