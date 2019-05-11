bca3400a430ac4b9857add71e482af37b78ee232
Kaai Suzuki <suzydp@me.com>
2019-05-11 15:31:11 -0700

    Try adding number on example2

--- example1.ts
+++ example1.ts
@@ -1,7 +1,4 @@
 //define types
-
-let exampleA: any; //you can apply any kind of type here
-
 //both side should match with the data type you defined
 //you can add optional data type by using `|`
 
@@ -10,6 +7,7 @@ const arrayExample = [1, 3, 4, 5];
 let example1: boolean | number = 35; 
 
 let example2: number;
+example2 = 35; // you can assign values without error even if you'll add here later on
 
 let example3: string = 'Hello World';
 
